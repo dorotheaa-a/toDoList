e05e871d43737045a6d77127e3987e8796d7672f
Kodzukenken <abigail.01k4w@gmail.com>
2025-04-11 09:48:09 +0700

    initialize backend node

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useRef} from "react"
+import { Link } from "react-router-dom"
+
+const Home = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    
+    return (
+        <section className="home">
+            
+        </section>
+    );
+};
+
+export default Home;
