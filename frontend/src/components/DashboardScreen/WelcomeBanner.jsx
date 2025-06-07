1959180ec5d80a395be1ea24725a2bb2f283c33e
Kodzukenken <abigail.01k4w@gmail.com>
2025-06-07 23:21:26 +0700

    editing functions

--- frontend/src/components/DashboardScreen/WelcomeBanner.jsx
+++ frontend/src/components/DashboardScreen/WelcomeBanner.jsx
@@ -4,6 +4,12 @@ import styles from "../../styles/DashboardScreen/WelcomeBanner.module.css";
 import banner from './duck.jpg';
 
 const WelcomeBanner = () => {
+
+  const getCurrentDate = () => {
+    const options = { year: 'numeric', month: 'long', day:'numeric'};
+    return new Date().toLocaleDateString('en-US', options);
+  }
+
   return (
     <section className={styles.welcomeSection}>
       <div className={styles.bannerContainer}>
@@ -15,7 +21,7 @@ const WelcomeBanner = () => {
         <div className={styles.overlay}>
           <div className={styles.content}>
             <h2 className={styles.title}>Welcome Back!</h2>
-            <p className={styles.date}>Today is October 12, 2023</p>
+            <p className={styles.date}>Today is {getCurrentDate()}</p>
             <button className={styles.newNoteButton}>Make a New Note</button>
           </div>
         </div>
