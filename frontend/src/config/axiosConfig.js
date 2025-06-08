4013b681352b0dd4ed0d0cc3e2121e5cd28215c2
Samuel Kevin <samuel.laluyan@student.sgu.ac.id>
2025-06-08 12:41:11 +0700

    Initial commit

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -2,25 +2,24 @@ import axios from "axios";
 import { API_URL } from "./endpoints";
 
 const axiosInstance = axios.create({
-    baseURL: API_URL,
-    headers: {
-      "ngrok-skip-browser-warning": "true",
-      "Content-Type": "application/json",
-    },
-  });
-  
-  axiosInstance.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+  baseURL: "http://localhost:8080",
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-  );
-  
-  export default axiosInstance;
-  
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
